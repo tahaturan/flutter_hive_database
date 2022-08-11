68f7a7f9d55f54543b6ca60c06e542b7da9f4668
Taha Turan <tahaaturann16@gmail.com>
2022-08-11 09:45:09 +0300

    verilerin sifrelenerek saklanmasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hive_database/model/ogrenci.dart';
 import 'package:flutter_hive_database/pages/home_page.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/adapters.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter("uygulama");
+  //*Sifreleme Encryted
+  const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+  var containsEncryptionKey = await secureStorage.containsKey(key: "key");
+
+  if (!containsEncryptionKey) {
+    var key = Hive.generateSecureKey();
+    await secureStorage.write(key: "key", value: base64UrlEncode(key));
+  }
+
+  var encryptionKey =
+      base64Url.decode(await secureStorage.read(key: "key") ?? "taha");
+
+  debugPrint("EncryptionKey : $encryptionKey");
+
+  var sifreliKutu = await Hive.openBox("ozel",
+      encryptionCipher: HiveAesCipher(encryptionKey));
+  await sifreliKutu.put("secret", "hive is cool");
+  await sifreliKutu.put("sifre", "123654");
+  debugPrint(sifreliKutu.get("secret"));
+  debugPrint(sifreliKutu.get("sifre"));
+
   await Hive.openBox("test");
   //*burada test adinda bir kutu ac dedik bu kutuda bizim verilerimiz saklanacak
   //! yoksa olustur varsada onu kullan demis oluyoruz
